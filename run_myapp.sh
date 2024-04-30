3d230bb866d0efc29dca7f7981f33f66b4054f85
lerachu <132816864+lerachu@users.noreply.github.com>
2024-04-30 21:00:33 +0300

    Delete run_myapp.sh

--- run_myapp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -v /mnt/c/Users/йа/Desktop/загрузка/info:/app/info -v /mnt/c/Users/йа/Desktop/загрузка/XML:/app/XML  -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix --rm zagruzka
